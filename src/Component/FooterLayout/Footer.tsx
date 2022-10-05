f9942eb222482d307acc9cfb045ab75984774ee5
Giang <giang.nguyen090891@gmail.com>
2022-10-05 23:49:09 +0700

    first commit

---
+++ src/Component/FooterLayout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function Footer({}: Props) {
+  return (
+    <div className='text-danger fs-1'>Footer</div>
+  )
+}
+
+export default Footer
---
+++ src/Component/HeaderLayout/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Header({}: Props) {
+  return (
+    <div className='text-warning fs-1'>
+      Header
+ 
+    </div>
+  )
+}
